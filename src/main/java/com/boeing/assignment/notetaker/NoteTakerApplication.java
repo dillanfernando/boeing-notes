43cd91b51ac2ac8d35a33a9451777c3c4558973a
Dillan Fernando <dfernando@gmail.com>
2018-05-05 13:48:35 -0700

    Inital commit

---
+++ src/main/java/com/boeing/assignment/notetaker/NoteTakerApplication.java
@@ -0,0 +1,12 @@
+package com.boeing.assignment.notetaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteTakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteTakerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/boeing/assignment/notetaker/NoteTakerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.boeing.assignment.notetaker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NoteTakerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
